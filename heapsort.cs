92a94b46b218c6acbd1283629a7c4cb413f1d2ed
delgerskhn <delgersaikhan2001@gmail.com>
2023-01-23 21:03:08 +0800

    heapsort

--- heapsort.cs
+++ heapsort.cs
@@ -1,31 +1,127 @@
 using System;
 
+public class Heap
+{
+
+    private int[] arr;
+    public int Size { get; private set; }
+    public Heap(int[] arr)
+    {
+        this.arr = arr;
+        this.Size = arr.Length;
+    }
+    public void BuildMaxHeap()
+    {
+
+        for (int i = Size - 1; i >= 0; i--)
+        {
+            MaxHeapify(i);
+        }
+    }
+
+
+    public void MinHeapify(int i)
+    {
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        int maxInd = i;
+
+        if (l < Size && -arr[l] > -arr[maxInd])
+            maxInd = l;
+        if (r < Size && -arr[r] > -arr[maxInd])
+            maxInd = r;
+
+        if (i != maxInd)
+        {
+            int temp = arr[i];
+            arr[i] = arr[maxInd];
+            arr[maxInd] = temp;
+
+            MinHeapify(maxInd);
+        }
+    }
+
+    public void MaxHeapify(int i)
+    {
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        int maxInd = i;
+        //zuun baruun uur gurvaasaa ali ihiih ni indexiig maxInd ruu hadgalna
+
+        if (l < Size && arr[l] > arr[maxInd])
+            maxInd = l;
+
+        if (r < Size && arr[r] > arr[maxInd])
+            maxInd = r;
+
+        if (i != maxInd)
+        {
+            int temp = arr[i];
+            arr[i] = arr[maxInd];
+            arr[maxInd] = temp;
+
+            MaxHeapify(maxInd);
+        }
+
+    }
+
+    public void HeapSort()
+    {
+        BuildMaxHeap();
+
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            Swap(0, i);
+            Size--;
+            MaxHeapify(0);
+        }
+    }
+
+    private void Swap(int i, int j)
+    {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    public void Print()
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int n = 10;
-
         int[] array = new int[10] {
-            16, 4, 10, 14, 7, 9, 3, 2, 8, 1
+            1,2,3,4,5,6,7,8,9,10
         };
 
+        var heap = new Heap(array);
         Console.WriteLine("Before max heapify:");
-        Console.WriteLine(string.Join(" ", array));
+        heap.Print();
 
-        MaxHeapify(array, 1);
+        heap.MaxHeapify(0);
 
         Console.WriteLine("After max heapify:");
-        Console.WriteLine(string.Join(" ", array));
+        heap.Print();
 
         Console.WriteLine("Before build max heap:");
-        Console.WriteLine(string.Join(" ", array));
+        heap.Print();
 
-        BuildMaxHeap(array);
+        heap.BuildMaxHeap();
 
         Console.WriteLine("After build max heap:");
-        Console.WriteLine(string.Join(" ", array));
+        heap.Print();
+
+
+        Console.WriteLine("After heap sort:");
+        heap.HeapSort();
+        heap.Print();
         // int[] arr1 = new int[14] {
         //     27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0
         // };
@@ -39,58 +135,5 @@ internal class Program
         // Console.WriteLine(string.Join(" ", arr1));
     }
 
-    private static void BuildMaxHeap(int[] arr)
-    {
 
-        for (int i = arr.Length - 1; i >= 0; i--)
-        {
-            MaxHeapify(arr, i);
-        }
-    }
-
-
-    private static void MinHeapify(int[] arr, int i)
-    {
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        int maxInd = i;
-
-        if (l < arr.Length && -arr[l] > -arr[maxInd])
-            maxInd = l;
-        if (r < arr.Length && -arr[r] > -arr[maxInd])
-            maxInd = r;
-
-        if (i != maxInd)
-        {
-            int temp = arr[i];
-            arr[i] = arr[maxInd];
-            arr[maxInd] = temp;
-
-            MinHeapify(arr, maxInd);
-        }
-    }
-
-    private static void MaxHeapify(int[] arr, int i)
-    {
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        int maxInd = i;
-        //zuun baruun uur gurvaasaa ali ihiih ni indexiig maxInd ruu hadgalna
-
-        if (l < arr.Length && arr[l] > arr[maxInd])
-            maxInd = l;
-
-        if (r < arr.Length && arr[r] > arr[maxInd])
-            maxInd = r;
-
-        if (i != maxInd)
-        {
-            int temp = arr[i];
-            arr[i] = arr[maxInd];
-            arr[maxInd] = temp;
-
-            MaxHeapify(arr, maxInd);
-        }
-
-    }
 }
